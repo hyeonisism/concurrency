012c24368e455e121e937f767aa30766d18a1967
Hyunjin Choi <hyeonisism@gmail.com>
2020-10-23 18:57:48 +0900

    Add basic thread

---
+++ concurrency-tutorial/src/main/java/basic/BasicThread.java
@@ -0,0 +1,13 @@
+package basic;
+
+
+/**
+ * @author hyeonisism
+ */
+public class BasicThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Thread running");
+    }
+}
